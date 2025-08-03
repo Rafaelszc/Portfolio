e7a42e9e8b23640d5a848413adcd43ee025af813
Rafaelszc <rafaelbjj84@gmail.com>
2025-08-02 23:14:41 -0300

    chore: add lists and map func on header and footer

--- src/pages/common/Footer.jsx
+++ src/pages/common/Footer.jsx
@@ -1,4 +1,20 @@
 export const Footer = () => {
+        const contacts = [
+        {name: "github", url: "https://github.com/Rafaelszc", imgUrl: "icons/github-mark.webp"},
+        {name: "linkedin", url: "https://linkedin.com/in/rafaelszc", imgUrl: "icons/linkedin.webp"},
+        {name: "gmail", url: "mailto:rafaelbjj84@gmail.com", imgUrl: "icons/gmail.webp"}
+    ]
+
+    const renderContacts = (c) => {
+        return (
+                <li>
+                    <a href={c.url} target="_blank">
+                        <img className="h-8 hover:-translate-y-1 transform transition duration-200" 
+                        src={c.imgUrl} alt={c.name}/>
+                    </a>
+                </li>
+        )
+    }
     return(
         <footer className="bg-primary-50 h-32 border-t border-t-white pr-8 pl-8  flex justify-between items-center">
             <ul className="list-none flex flex-col gap-2">
@@ -7,24 +23,9 @@ export const Footer = () => {
                 <li className="text-white">MIT License</li>
             </ul>
             <ul className="flex list-none gap-6">
-                <li>
-                    <a href="https://github.com/Rafaelszc" target="_blank">
-                        <img className="h-8 hover:-translate-y-1 transform transition duration-200" 
-                        src="/icons/github-mark.webp" />
-                    </a>
-                </li>
-                <li>
-                    <a href="https://linkedin.com/in/rafaelszc" target="_blank">
-                        <img className="h-8 hover:-translate-y-1 transform transition duration-200" 
-                        src="/icons/linkedin.webp" alt="" />
-                    </a>
-                </li>
-                <li>
-                    <a href="mailto:rafaelbjj84@gmail.com" target="_blank">
-                        <img className='h-8 hover:-translate-y-1 transform transition duration-200' 
-                        src="icons/gmail.webp" alt="" />
-                    </a>
-                </li>
+                {contacts.map((c) => (
+                    renderContacts(c)
+                ))}
             </ul>
         </footer>
     )
--- src/pages/common/Header.jsx
+++ src/pages/common/Header.jsx
@@ -1,8 +1,39 @@
 export const Header = () => {
+    const contacts = [
+        {name: "github", url: "https://github.com/Rafaelszc", imgUrl: "icons/github-mark.webp"},
+        {name: "linkedin", url: "https://linkedin.com/in/rafaelszc", imgUrl: "icons/linkedin.webp"},
+        {name: "gmail", url: "mailto:rafaelbjj84@gmail.com", imgUrl: "icons/gmail.webp"}
+    ]
+    const sections = [
+        {name: "About", href: "#about-me"},
+        {name: "Projects", href: "#projects"},
+        {name: "Skills", href: "#skills"}
+    ]
+
+
+    const renderContacts = (c) => {
+        return (
+                <li>
+                    <a href={c.url} target="_blank">
+                        <img className="h-6 hover:-translate-y-1 transform transition duration-200" 
+                        src={c.imgUrl} alt={c.name}/>
+                    </a>
+                </li>
+        )
+    }
+
+    const renderSections = (s) => {
+        return (
+            <li className="font-medium hover:-translate-y-1 transform transition duration-200 hover:text-white">
+                <a href={s.href}>{s.name}</a>
+            </li>
+        )
+    }
+
     return (
         <header className="h-md-header flex items-center justify-between pr-8 pl-8 sticky top-0 bg-primary-100 z-50">
             <div>
-                <a href="/">
+                <a href="/Portifolio">
                     <h1 className="select-none animate-typing border-r-2 border-r-white overflow-hidden text-3xl text-white font-medium">
                         Rafaelszc
                     </h1>
@@ -11,37 +42,16 @@ export const Header = () => {
 
             <nav>
                 <ul className="flex list-none gap-4 text-purple-500">
-                    <li className="font-medium hover:-translate-y-1 transform transition duration-200 hover:text-white">
-                        <a href="#about-me">About</a>
-                    </li>
-                    <li className="font-medium hover:-translate-y-1 transform transition duration-200 hover:text-white">
-                        <a href="#projects">Projects</a>
-                    </li>
-                    <li className="font-medium hover:-translate-y-1 transform transition duration-200 hover:text-white">
-                        <a href="#skills">Skills</a>
-                    </li>
+                    {sections.map((s) => (
+                        renderSections(s)
+                    ))}
                 </ul>
             </nav>
             <div>
                 <ul className="flex list-none gap-4">
-                    <li>
-                        <a href="https://github.com/Rafaelszc" target="_blank">
-                            <img className="h-6 hover:-translate-y-1 transform transition duration-200" 
-                            src="/icons/github-mark.webp" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="https://linkedin.com/in/rafaelszc" target="_blank">
-                            <img className="h-6 hover:-translate-y-1 transform transition duration-200" 
-                            src="/icons/linkedin.webp" alt="" />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="mailto:rafaelbjj84@gmail.com" target="_blank">
-                            <img className='h-6 hover:-translate-y-1 transform transition duration-200' 
-                            src="icons/gmail.webp" alt="" />
-                        </a>
-                    </li>
+                    {contacts.map((c) => (
+                        renderContacts(c)
+                    ))}
                 </ul>
             </div>
         </header>
