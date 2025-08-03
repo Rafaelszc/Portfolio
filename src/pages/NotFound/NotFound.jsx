79c99c6a342534e3cb6c7676a28feabf7a070331
Rafaelszc <rafaelbjj84@gmail.com>
2025-08-02 23:19:48 -0300

    feat: add Link from react router to link homepage

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const NotFound = () => {
   return (
     <div className=" h-screen ml-40 flex flex-col justify-center text-2xl text-white ">
-      <span>Page not found :(((</span>
+      <span>Error 404 page not found :(((</span>
         
       <span>
-        Return to <a className="text-purple-500" href="/"> Home</a>
+        Return to <Link to="/" className="text-purple-500"> Home </Link>
       </span>
     </div>  
   );
